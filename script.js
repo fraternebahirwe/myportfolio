8be51677edde33211ab850c6e51fc00be8d505bb
Fraterne Bahirwe <fraterne2023@gmail.com>
2026-06-09 16:56:53 +0200

    put all styles on the page

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-const themeBtn = document.querySelector(".theme-btn");
+
+
+
 const burgerBtn = document.querySelector(".burger-btn");
 const navLinks = document.querySelector(".nav-links");
 
-if (themeBtn) {
-  themeBtn.addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
-
-    if (document.body.classList.contains("light-mode")) {
-      themeBtn.textContent = "☾";
-    } else {
-      themeBtn.textContent = "☼";
-    }
-  });
-}
 
 if (burgerBtn && navLinks) {
   burgerBtn.addEventListener("click", () => {
